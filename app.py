e32bbb99d9b8245461caac12c84cbcabefff13ad
aleg-m <aleg.manasi@gmail.com>
2026-04-07 17:04:59 -0500

    added app and data file

---
+++ app.py
@@ -0,0 +1,5 @@
+#this is where the main application content goes
+import pandas as pd
+from datetime import date
+import math
+from data import load_patients, get_patient_visits, get_all_patient_ids, add_visit
